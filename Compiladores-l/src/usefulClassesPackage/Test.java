193d4038487d78e1a98e91f3dc8f85e82dd0037c
Thomas O <thomasojeda05@gmail.com>
2020-09-28 16:25:05 -0300

    El nombre de la clase usefulClassesPackage estaba mal

--- Compiladores-l/src/usefulClassesPackage/Test.java
+++ Compiladores-l/src/usefulClassesPackage/Test.java
@@ -9,9 +9,9 @@ import lexicalAnalyzerPackage.LexicalAnalyzer;
 
 public class Test {
 
-	//public static final String BASE_PATH = "/home/chequeado/Documentos/Facultad/Compiladores/Compiladores-I/Compiladores-l/src/usefullClassesPackage/";
+	//public static final String BASE_PATH = "/home/chequeado/Documentos/Facultad/Compiladores/Compiladores-I/Compiladores-l/src/usefulClassesPackage/";
 	public static final String BASE_PATH = "C:\\Users\\Thomas\\git\\Compiladores-I\\Compiladores-l\\src\\usefulClassesPackage\\";
-	//public static final String BASE_PATH = "C:\\Users\\Cio\\git\\Compiladores-I\\Compiladores-l\\src\\usefullClassesPackage\\";
+	//public static final String BASE_PATH = "C:\\Users\\Cio\\git\\Compiladores-I\\Compiladores-l\\src\\usefulClassesPackage\\";
 	
 	public Test() throws IOException {
 		test("program3.txt");
